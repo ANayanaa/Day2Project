58533f5d59cea61c67a50b95410c0052c09ae738
aryanjairath <45392102+aryanjairath@users.noreply.github.com>
2018-12-02 00:27:48 -0500

    PartnerOne part

--- Day2Project.java
+++ Day2Project.java
@@ -1,6 +1,34 @@
 
 public class Day2Project {
     public static void main (String[]args){
+        import java.util.ArrayList;
+	public class MyPart{
+		public static void main(String []args){
+
+						 convertToArrayList(9018);
+		}
+		public static void convertToArrayList(int x){
+						ArrayList<Integer> list = new ArrayList<>();
+
+						int intSize = 0;
+
+						int temp = x;
+
+						while(temp > 0){
+							intSize++;
+							list.add(0);
+							temp /= 10;
+						}
+
+						while(x > 0){
+							list.set(intSize - 1, x % 10);
+							x /= 10;
+							intSize--;
+							}
+
+						System.out.print(list);
+					}
+					}
         
       
     }  
