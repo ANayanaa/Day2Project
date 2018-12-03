2b98733f8ddd4912baf0769570d59f794fc272d1
Arnitha <arnithan@Arnithas-Air.fios-router.home>
2018-12-02 21:27:14 -0500

    Update Day2Project.java

--- Day2Project.java
+++ Day2Project.java
@@ -7,8 +7,29 @@ public class Day2Project {
         list1.add(1);
         list1.add(8);
         
-        
+        convertToArrayList(9018);
         arrayListToInt(list1);
+    }
+    public static void convertToArrayList(int x){
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        int intSize = 0;
+        
+        int temp = x;
+        
+        while(temp > 0){
+            intSize++;
+            list.add(0);
+            temp /= 10;
+        }
+        
+        while(x > 0){
+            list.set(intSize - 1, x % 10);
+            x /= 10;
+            intSize--;
+        }
+        
+        System.out.print(list);
     }
     
     public static void arrayListToInt(ArrayList<Integer>list1){
