c3fe74fb9d73f864735c26fba35d3071499783bc
Arnitha <arnithan@Arnithas-Air.fios-router.home>
2018-12-02 21:26:02 -0500

    Partner2 Work

--- Day2Project.java
+++ Day2Project.java
@@ -1,35 +1,33 @@
-
+import java.util.ArrayList;
 public class Day2Project {
-    public static void main (String[]args){
-        import java.util.ArrayList;
-	public class MyPart{
-		public static void main(String []args){
-
-						 convertToArrayList(9018);
-		}
-		public static void convertToArrayList(int x){
-						ArrayList<Integer> list = new ArrayList<>();
-
-						int intSize = 0;
-
-						int temp = x;
-
-						while(temp > 0){
-							intSize++;
-							list.add(0);
-							temp /= 10;
-						}
-
-						while(x > 0){
-							list.set(intSize - 1, x % 10);
-							x /= 10;
-							intSize--;
-							}
-
-						System.out.print(list);
-					}
-					}
+    public static void main(String []args){
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(9);
+        list1.add(0);
+        list1.add(1);
+        list1.add(8);
         
-      
-    }  
+        
+        arrayListToInt(list1);
+    }
+    
+    public static void arrayListToInt(ArrayList<Integer>list1){
+        int t = list1.size();
+        int l =1;
+        int num = 0;
+        for(int i = 0; i<list1.size(); i++){
+            for(int j =1; j<t; j++){
+                
+                l*=10;
+                
+            }
+            
+            num+=( list1.get(i))*l;
+            t--;
+            l=1;
+            
+            
+        }
+        System.out.print(num);
+    }
 }
